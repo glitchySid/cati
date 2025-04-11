46aa6ac2541c1fe70d8665aa47842056fb7f519b
glitchySid <siddheshmhatre2005@gmail.com>
2025-04-11 18:34:41 +0530

    Add null check before formatting and printing links

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,11 @@ fn main() -> Result<()> {
     let input_image_path = Path::new("test2.png");
     let text = get_text(input_image_path)?;
     let links = extract_links(&text);
-    let output = format_links_table(&links);
-    println!("links \n{}", output);
+
+    if !links.is_empty() {
+        let output = format_links_table(&links);
+        println!("links \n{}", output);
+    }
     if let Err(e) = display_with_bat(&text) {
         eprintln!("Error displaying Rust code: {}", e);
     }
